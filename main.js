3691f505d400c66986a6bf22226392ce1a56f31b
StBran0912 <95239081+StBran0912@users.noreply.github.com>
2022-09-08 11:23:59 +0200

    Ingerface eingefügt

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 // @ts-check
 import * as lb2d from './lb2d.js';
 
+/** Interface Box Properties
+ * @typedef {Object} BoxProps
+ * @property {lb2d.Vector[]} vertices
+ * @property {lb2d.Vector} location
+ * @property {lb2d.Vector} velocity
+ * @property {number} angVelocity
+ * @property {number} mass
+ * @property {number} inertia
+ * @property {number} coefficient
+ */
 
 /** Interface Box
  * @typedef {Object} Box
- * @property {lb2d.Vector[]} vertices
- * @property {lb2d.Vector} location
- * @property {{velocity:lb2d.Vector, angVelocity:number, mass:number, inertia:number, coefficient:number} } props
+ * @property {BoxProps} props
  * @property {() => void} display
  * @property {(angle: number) => void} rotate
 */
@@ -15,18 +23,10 @@ import * as lb2d from './lb2d.js';
  * @type {(posx: number, posy: number, w: number, h: number) => Box} 
  */
 function createBox(posx, posy, w, h) {
-    /** 
-    * @type {lb2d.Vector[]} vertices 
-    */
-    const vertices = new Array(5);
-    vertices[0] = lb2d.createVector(posx, posy);
-    vertices[1] = lb2d.createVector(posx + w, posy);
-    vertices[2] = lb2d.createVector(posx + w, posy + h);
-    vertices[3] = lb2d.createVector(posx, posy + h);
-    vertices[4] = vertices[0];
-    const location = lb2d.createVector(posx + w / 2, posy + h / 2);
-
+   /** @type {BoxProps} */
     const props = {
+        vertices: new Array(5),
+        location: lb2d.createVector(posx + w / 2, posy + h / 2),
         velocity: lb2d.createVector(0, 0),
         angVelocity: 0,
         mass: (w + h)*2,
@@ -34,12 +34,18 @@ function createBox(posx, posy, w, h) {
         coefficient: 0.0005
     }
 
+    props.vertices[0] = lb2d.createVector(posx, posy);
+    props.vertices[1] = lb2d.createVector(posx + w, posy);
+    props.vertices[2] = lb2d.createVector(posx + w, posy + h);
+    props.vertices[3] = lb2d.createVector(posx, posy + h);
+    props.vertices[4] = props.vertices[0];
+
     /** rotate
      * @type {(angle: number) => void} 
     */
     function rotate(angle) {
         for (let i = 0; i < 4; i++) {
-            vertices[i].rotateMatrix(location, angle);
+            props.vertices[i].rotateMatrix(props.location, angle);
         }
     }
 
@@ -48,13 +54,11 @@ function createBox(posx, posy, w, h) {
     */
     function display() {
         lb2d.strokeWidth(2);
-        lb2d.shape(vertices[0].pos.x, vertices[0].pos.y, vertices[1].pos.x, vertices[1].pos.y, vertices[2].pos.x, vertices[2].pos.y, vertices[3].pos.x, vertices[3].pos.y, 0);
-        lb2d.circle(location.pos.x, location.pos.y, 2, 0);
+        lb2d.shape(props.vertices[0].pos.x, props.vertices[0].pos.y, props.vertices[1].pos.x, props.vertices[1].pos.y, props.vertices[2].pos.x, props.vertices[2].pos.y, props.vertices[3].pos.x, props.vertices[3].pos.y, 0);
+        lb2d.circle(props.location.pos.x, props.location.pos.y, 2, 0);
     }
 
     return {
-        vertices:vertices,
-        location:location,
         props:props,
         rotate:rotate,
         display:display
