0c68a6f14456ea87be6e6625901c1deba977c09b
StBran0912 <95239081+StBran0912@users.noreply.github.com>
2022-09-18 18:37:06 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,36 +2,52 @@
 import * as lb2d from './lb2d.js';
 import * as phys from './physics.js';
 
+/** Interface Kicking
+ * @typedef {Object} Kicking
+ * @property {{index: number|null, base: lb2d.Vector}} kick
+ * @property {() => void} check
+ */
+
+/** @type {(el: (phys.Box|phys.Ball)[] ) => Kicking} */
+function createKicking(el) {
+    
+    /** @type {{index: number|null, base: lb2d.Vector}} */
+    let kick = {
+        index: null,
+        base: lb2d.createVector(0, 0)
+    }
+    
+    function check() {
+        if (lb2d.isMouseDown() && kick.index == null) {
+            el.forEach((element, index) => {
+                if (element.me.location.dist(lb2d.createVector(lb2d.mouseX, lb2d.mouseY)) < element.me.mass) {
+                  kick.base.set(element.me.location.pos.x, element.me.location.pos.y);
+                  kick.index = index;
+                }
+            })    
+        }
+    
+        if (lb2d.isMouseDown() && kick.index != null) {
+            lb2d.drawArrow(kick.base, lb2d.createVector(lb2d.mouseX, lb2d.mouseY), 100);
+        }  
+    
+        if (lb2d.isMouseUp() && kick.index != null) {
+            let mouse = lb2d.createVector(lb2d.mouseX, lb2d.mouseY);
+            let force = lb2d.subVector(mouse, el[kick.index].me.location);
+            el[kick.index].applyForce(force, 0);
+            kick.index = null;
+        }      
+    }
+
+    return {
+        kick,
+        check
+    }
+}
+
 /** @type {(phys.Box|phys.Ball)[]} */
 let el = [];
-
-/** @type {{index: number|null, base: lb2d.Vector}} */
-let arrow = {
-    index: null,
-    base: lb2d.createVector(0, 0)
-}
-
-function CheckMouseKicking() {
-    if (lb2d.isMouseDown() && arrow.index == null) {
-        el.forEach((element, index) => {
-            if (element.me.location.dist(lb2d.createVector(lb2d.mouseX, lb2d.mouseY)) < element.me.mass) {
-              arrow.base.set(element.me.location.pos.x, element.me.location.pos.y);
-              arrow.index = index;
-            }
-        })    
-    }
-
-    if (lb2d.isMouseDown() && arrow.index != null) {
-        lb2d.drawArrow(arrow.base, lb2d.createVector(lb2d.mouseX, lb2d.mouseY), 100);
-    }  
-
-    if (lb2d.isMouseUp() && arrow.index != null) {
-        let mouse = lb2d.createVector(lb2d.mouseX, lb2d.mouseY);
-        let force = lb2d.subVector(mouse, el[arrow.index].me.location);
-        el[arrow.index].applyForce(force, 0);
-        arrow.index = null;
-    }
-}
+let kicking = createKicking(el);
 
 function start() {    
     el.push(phys.createBox(200, 100, 110, 70));
@@ -46,7 +62,7 @@ function start() {
 
 function draw() {
     lb2d.background(50, 50, 50);
-    CheckMouseKicking();
+    kicking.check();
     for (let i = 0; i < el.length; i++) {
         for (let j = i+1; j < el.length; j++ ) {
             phys.checkCollision(el[i], el[j]);
